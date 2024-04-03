9ce413fd5a51cedc961486f942c3ab582492efcf
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-04-03 11:57:36 +0530

    and commit

---
+++ and.sql
@@ -0,0 +1,9 @@
+SELECT column1, column2
+FROM table_name
+WHERE condition1=something1 AND condition2=something2 AND condition3=something3;
+-- CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+-- 1            Alfreds Futt     ste	    Str. 57	Berlin	12209	    Germany
+-- 2        	Ana Trujillo     aredado    2222	México  D.F.05021	Mexico
+-- 3        	Antonio More     aquería    2312	México  D.F.05023	Mexico
+-- 4            Around the H     Thomas  	120 Sq.	London	WA1 1DP	    UK
+-- 5        	Berglunds sn     öp	Chri    8	    Luleå	S-958 22	Sweden
