323de0205f170d404f1cdd45c5a7b8b28b1d995a
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-04-03 13:14:17 +0530

    not commit

---
+++ not.sql
@@ -0,0 +1,23 @@
+SELECT column1, column2
+FROM table_name
+WHERE NOT condition=something;
+-- CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+-- 1            Alfreds Futt     ste	    Str. 57	Berlin	12209	    Germany
+-- 2        	Ana Trujillo     aredado    2222	México  D.F.05021	Mexico
+-- 3        	Antonio More     aquería    2312	México  D.F.05023	Mexico
+-- 4            Around the H     Thomas  	120 Sq.	London	WA1 1DP	    UK
+-- 5        	Berglunds sn     öp	Chri    8	    Luleå	S-958 22	Sweden
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'A%';
+
+SELECT * FROM Customers
+WHERE CustomerID NOT BETWEEN 10 AND 60;
+
+SELECT * FROM Customers
+WHERE City NOT IN ('Paris', 'London');
+
+SELECT * FROM Customers
+WHERE NOT CustomerID > 50;
+
+SELECT * FROM Customers
+WHERE NOT CustomerId < 50;
